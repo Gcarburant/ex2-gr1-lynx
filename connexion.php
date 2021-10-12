543e9f5bf11d30c5e33812e51491a219bb5085ba
chris <e1916303@cmaisonneuve.qc.ca>
2021-10-12 12:50:21 -0400

    test connexion

---
+++ connexion.php
@@ -0,0 +1,8 @@
+voici l'info dans le formulaire
+
+<?php
+    echo '<hr>';
+    echo $_POST['courriel'];
+    echo '<hr>';
+    echo $_POST['mdp'];
+?>
--- demarrer-serveur.bat
+++ demarrer-serveur.bat
@@ -1,2 +1,2 @@
 REM Ajustez cette commande à vos besoin avant de l'utiliser !
-E:\logiciels\php8010\php -S 127.0.0.1:81
+E:\session_5\web\tim_logi\php-8.0.10-nts-Win32-vs16-x64\php -S 127.0.0.1:81
